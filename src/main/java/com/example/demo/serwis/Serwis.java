100c77a6ce0d61106177aa162ce10a6173393358
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-11-10 15:35:32 +0100

    Save events in map

--- src/main/java/com/example/demo/serwis/Serwis.java
+++ src/main/java/com/example/demo/serwis/Serwis.java
@@ -1,6 +1,7 @@
 package com.example.demo.serwis;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.core.io.Resource;
 
@@ -8,6 +9,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
 import org.jsoup.nodes.Document;
 
 @org.springframework.stereotype.Service
@@ -17,14 +20,21 @@ public class Serwis {
     public File getCalendar() throws IOException {
 
         Document document;
-        Elements days,events;
+        Elements events, days;
+        Map<String, String> map = new HashMap();
+        int i;
 
         document = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + "2020" + "&miesiac=" + "10" + "&lang=1").get();
 
         events= document.select("div.InnerBox");
-        System.out.println(events);
         days = document.select("a.active");
-        System.out.println(days);
+
+        for(i=0; i < events.size(); i++) {
+            System.out.println(days.get(i).text());
+            System.out.println(events.get(i).text());
+            map.put(days.get(i).text(), events.get(i).text());
+        }
+
 
         return new File("test");
     }
