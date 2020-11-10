f36f3f16e9094e841014467e42df430a7794b4ce
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-11-10 14:46:50 +0100

    Created class

--- src/main/java/com/example/demo/restapi/Restapi.java
+++ src/main/java/com/example/demo/restapi/Restapi.java
@@ -1,9 +1,30 @@
 package com.example.demo.restapi;
 
+import com.example.demo.serwis.Serwis;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/")
 public class Restapi {
+
+    private Serwis serwis;
+
+    @Autowired
+    public Restapi(Serwis serwis) {
+        this.serwis = serwis;
+    }
+
+    @GetMapping("calendar")
+    public File getCalendarEvents() throws IOException {
+        return serwis.getCalendar();
+    }
+
 }
--- src/main/java/com/example/demo/serwis/Serwis.java
+++ src/main/java/com/example/demo/serwis/Serwis.java
@@ -1,9 +1,15 @@
 package com.example.demo.serwis;
 
+import org.springframework.core.io.Resource;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 @org.springframework.stereotype.Service
 
 public class Serwis {
+    public File getCalendar() {
+        return new File("test");
+    }
 }
