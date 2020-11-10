c93a3af675566055978b757541153f5585c15a82
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-11-10 14:31:10 +0100

    Initialization project

---
+++ src/main/java/com/example/demo/restapi/Restapi.java
@@ -0,0 +1,9 @@
+package com.example.demo.restapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class Restapi {
+}
---
+++ src/main/java/com/example/demo/serwis/Serwis.java
@@ -0,0 +1,9 @@
+package com.example.demo.serwis;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@org.springframework.stereotype.Service
+
+public class Serwis {
+}
