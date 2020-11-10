0c114e1495be5daecb6239a8281de269a9b98929
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-11-10 14:59:34 +0100

    Get page

--- src/main/java/com/example/demo/serwis/Serwis.java
+++ src/main/java/com/example/demo/serwis/Serwis.java
@@ -1,15 +1,24 @@
 package com.example.demo.serwis;
 
+import org.jsoup.Jsoup;
 import org.springframework.core.io.Resource;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import org.jsoup.nodes.Document;
 
 @org.springframework.stereotype.Service
 
+
 public class Serwis {
-    public File getCalendar() {
+    public File getCalendar() throws IOException {
+
+        Document document;
+        document = Jsoup.connect("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + "2020" + "&miesiac=" + "10" + "&lang=1").get();
+        System.out.println(document);
+
         return new File("test");
     }
 }
