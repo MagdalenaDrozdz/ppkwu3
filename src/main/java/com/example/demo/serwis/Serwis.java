a8b38439a02317d53b60c7ec59c156f360cad3c2
Magdalena Drozdz <215707@edu.p.lodz.pl>
2020-11-24 15:05:26 +0100

    Refactoring

--- src/main/java/com/example/demo/serwis/Serwis.java
+++ src/main/java/com/example/demo/serwis/Serwis.java
@@ -38,15 +38,12 @@ public class Serwis {
         days = document.select("a.active");
 
         for(i=0; i < events.size(); i++) {
-            System.out.println(days.get(i).text());
-            System.out.println(events.get(i).text());
             map.put(Integer.parseInt(days.get(i).text()), events.get(i).text());
         }
 
         for(Integer key : map.keySet()) {
             VEvent event = new VEvent();
             event.setSummary(map.get(key));
-            System.out.println("Key:" + key + "Value:" + map.get(key)) ;
             day.set(y,  m - 1 , key);
             event.setDateStart(day.getTime());
             event.setDateEnd(day.getTime());
